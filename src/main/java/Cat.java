aa7eb6ea5fb48ed8d4efca00eed461d405f7a97f
Yuri <yuriidenisov@gmail.com>
2024-07-17 07:31:33 +0500

    all done

---
+++ src/main/java/Cat.java
@@ -0,0 +1,12 @@
+public class Cat {
+    private String catMessage;
+
+
+    public String getMessage() {
+        return catMessage;
+    }
+
+    public void setMessage(String catMessage) {
+        this.catMessage = catMessage;
+    }
+}
